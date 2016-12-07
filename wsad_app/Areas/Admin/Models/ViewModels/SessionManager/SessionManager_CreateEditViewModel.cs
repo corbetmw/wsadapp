5eb0f2025902e37d5adda776c0f708cbe94fd3b8
Matthew Corbett <corbetmw@mail.uc.edu>
2016-12-07 01:59:58 -0500

    Created drop down menus for the admins

---
+++ wsad_app/Areas/Admin/Models/ViewModels/SessionManager/SessionManager_CreateEditViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using wsad_app.Models.DataAccess;
+
+namespace wsad_app.Areas.Admin.Models.ViewModels.SessionManager
+{
+    public class SessionManager_CreateEditViewModel
+    {
+    }
+
+    public SessionManager_CreateEditViewModel(Session sessionDTO)
+    {
+        Id = sessionDTO.Id;
+        UserName = sessionDTO.UserName;
+        FirstName = sessionDTO.FirstName;
+        LastName = sessionDTO.LastName;
+        EmailAddress = sessionDTO.EmailAddress;
+        EmailOpt = sessionDTO.EmailOpt;
+    }
+}
